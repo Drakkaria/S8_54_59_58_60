b350e0afde1c5b94d095f7743bd6a6c865ca0c5c
akaya <darkness_ftv@mail.ru>
2022-12-30 02:23:32 +0800

    53

---
+++ dz53/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(1, n));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return (start + " " + PrintNumbers(start + 1, end)); // "3  2  1"
+}
